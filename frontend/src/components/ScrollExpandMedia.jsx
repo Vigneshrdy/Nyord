a3687b0edaa6d7bd012483977af7a38de675477d
a-hansika <a.redlahansika@gmail.com>
2025-12-03 19:52:37 +0530

    hero

--- frontend/src/components/ScrollExpandMedia.jsx
+++ frontend/src/components/ScrollExpandMedia.jsx
@@ -116,7 +116,7 @@ const ScrollExpandMedia = ({
   return (
     <div
       ref={sectionRef}
-      className='transition-colors duration-700 ease-in-out overflow-x-hidden'
+      className='transition-colors duration-700 ease-in-out overflow-x-hidden bg-black'
     >
       <section className='relative flex flex-col items-center justify-start min-h-[100dvh]'>
         <div className='relative w-full flex flex-col items-center min-h-[100dvh]'>
